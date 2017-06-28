fe464d91d314749eac271d2a0e658154d34d0ecc
heaventouch <549784439@qq.com>
2017-06-28 20:05:56 +0800

    Update adapter-pattern.py

--- adapter pattern/adapter-pattern.py
+++ adapter pattern/adapter-pattern.py
@@ -1,1 +1,45 @@
 
+class Duck(object):
+    def quack(self):
+        pass
+    
+    def fly(self):
+        pass
+
+class MallardDuck(Duck):
+    def quack(self):
+        print("Quck")
+        
+    def fly(self):
+        print("I am flying")
+
+class Turkey(object):
+    def gobble(self):
+        pass
+    
+    def fly(self):
+        pass
+
+class WileTurkey(Turkey):
+    def gobble(self):
+        print("Gobble")
+    
+    def fly(self):
+        print("I am flying a short distance")
+
+class TurkeyAdapter(Duck):
+    def __init__(self, turkey):
+        self.turkey = turkey
+    
+    def quack(self):
+        self.turkey.gobble()
+    
+    def fly(self):
+        for i in range(5):
+            self.turkey.fly()
+
+def TestTurkeyAdapter():
+    turkey = WileTurkey()
+    turkeyAdapter = TurkeyAdapter(turkey)
+    turkeyAdapter.quack()
+    turkeyAdapter.fly()
